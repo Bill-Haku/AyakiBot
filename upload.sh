10d55b6f42ca370ccf29e792abe6a6a50073f685
Bill-Haku <battleandwar@hotmail.com>
2022-07-14 10:23:28 +0800

    feat: write shell to upload script

---
+++ upload.sh
@@ -0,0 +1,3 @@
+scp ~/pycharmprojects/qqpixivbot/main.py hakubill@bill.debian:~/ayaki
+scp ~/pycharmprojects/qqpixivbot/features.py hakubill@bill.debian:~/ayaki
+scp ~/pycharmprojects/qqpixivbot/events.txt hakubill@bill.debian:~/ayaki
