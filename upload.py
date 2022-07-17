607e021adf918646fb4d390ded862a3a76ce77f8
Bill-Haku <battleandwar@hotmail.com>
2022-07-17 22:31:11 +0800

    feat: recover 4.0.7

--- upload.py
+++ upload.py
@@ -86,53 +86,53 @@ def _upload_pixiv_image():
         with open("pixiv_%s.jpg" % cur_Illust.id, 'wb') as f:
             file_name = "pixiv_%s.jpg" % cur_Illust.id
             f.write(res.content)
-#            encoded_str = base64.b64encode(res.content)
-#            data = {
-#                "source": encoded_str,
-#                "action": "upload",
-#                "key": "26c556d135a0f1c18048fbac0d9b85c8",
-#                "format": "txt"
-#            }
-#            post_url = "http://nas.hakubill.tech:1234/api/1/upload"
-#            chevereto_req = requests.post(post_url, verify=False, data=data)
-#            img_url = str(chevereto_req.content, 'utf-8')
-#            print(img_url)
-#
-#            # 检查图片大小
-#            size = os.path.getsize(file_name)
-#            size /= 1024  # 单位化为kB
-#            print(size)
-#            img_compressed_url = img_url
-#            if size > 4000:
-#                # 文件过大，额外保存一个压缩版
-#                file_name_compressed = "pixiv_%s_compressed.png" % cur_Illust.id
-#                try:
-#                    compression(file_name, file_name_compressed)
-#                    try:
-#                        with open(file_name_compressed, "rb") as f_compressed:
-#                            encoded_str = base64.b64encode(f_compressed.read())
-#                        data = {
-#                            "source": encoded_str,
-#                            "action": "upload",
-#                            "key": "26c556d135a0f1c18048fbac0d9b85c8",
-#                            "format": "txt"
-#                        }
-#                        post_url = "http://nas.hakubill.tech:1234/api/1/upload"
-#                        chevereto_req = requests.post(post_url, verify=False, data=data)
-#                        img_compressed_url = str(chevereto_req.content, 'utf-8')
-#                        print("compressed " + img_compressed_url)
-#                    except Exception as err:
-#                        print("save compress fail " + str(err))
-#                except Exception as err:
-#                    print("compress fail: " + str(err))
-#                    continue
-#
-#            # 写入csv文件
-#            # csv文件格式: "id", "img_url", "img_compressed_url", "have_used?", "title"
-#            with open("pixiv_src.csv", "a+") as w:
-#                csv_line = str(cur_Illust.id) + "," + img_url + "," + img_compressed_url + ",0," + \
-#                           str(cur_Illust.title) + "," + author + "\n"
-#                w.write(csv_line)
+            encoded_str = base64.b64encode(res.content)
+            data = {
+                "source": encoded_str,
+                "action": "upload",
+                "key": "26c556d135a0f1c18048fbac0d9b85c8",
+                "format": "txt"
+            }
+            post_url = "http://nas.hakubill.tech:1234/api/1/upload"
+            chevereto_req = requests.post(post_url, verify=False, data=data)
+            img_url = str(chevereto_req.content, 'utf-8')
+            print(img_url)
+
+            # 检查图片大小
+            size = os.path.getsize(file_name)
+            size /= 1024  # 单位化为kB
+            print(size)
+            img_compressed_url = img_url
+            if size > 4000:
+                # 文件过大，额外保存一个压缩版
+                file_name_compressed = "pixiv_%s_compressed.png" % cur_Illust.id
+                try:
+                    compression(file_name, file_name_compressed)
+                    try:
+                        with open(file_name_compressed, "rb") as f_compressed:
+                            encoded_str = base64.b64encode(f_compressed.read())
+                        data = {
+                            "source": encoded_str,
+                            "action": "upload",
+                            "key": "26c556d135a0f1c18048fbac0d9b85c8",
+                            "format": "txt"
+                        }
+                        post_url = "http://nas.hakubill.tech:1234/api/1/upload"
+                        chevereto_req = requests.post(post_url, verify=False, data=data)
+                        img_compressed_url = str(chevereto_req.content, 'utf-8')
+                        print("compressed " + img_compressed_url)
+                    except Exception as err:
+                        print("save compress fail " + str(err))
+                except Exception as err:
+                    print("compress fail: " + str(err))
+                    continue
+
+            # 写入csv文件
+            # csv文件格式: "id", "img_url", "img_compressed_url", "have_used?", "title"
+            with open("pixiv_src.csv", "a+") as w:
+                csv_line = str(cur_Illust.id) + "," + img_url + "," + img_compressed_url + ",0," + \
+                           str(cur_Illust.title) + "," + author + "\n"
+                w.write(csv_line)
 
 
 
