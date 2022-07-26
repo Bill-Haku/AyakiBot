878a275a6470d56d21fa88e86906650913263c24
Bill-Haku <battleandwar@hotmail.com>
2022-07-26 15:30:19 +0800

    code: rewrite upload.sh

--- upload.sh
+++ upload.sh
@@ -1,4 +1,12 @@
-scp ~/pycharmprojects/qqpixivbot/main.py hakubill@bill.debian:~/ayaki
-scp ~/pycharmprojects/qqpixivbot/features.py hakubill@bill.debian:~/ayaki
-scp ~/pycharmprojects/qqpixivbot/config.yaml hakubill@bill.debian:~/ayaki
-scp ~/pycharmprojects/qqpixivbot/private_config.yaml hakubill@bill.debian:~/ayaki
+#!/bin/zsh
+
+local_path=~/pycharmprojects/qqpixivbot
+# shellcheck disable=SC2088
+remote_path="~/ayaki"
+remote_username="hakubill"
+remote_host="bill.debian"
+
+scp ${local_path}/main.py ${remote_username}@${remote_host}:${remote_path}
+scp ${local_path}/features.py ${remote_username}@${remote_host}:${remote_path}
+scp ${local_path}/config.yaml ${remote_username}@${remote_host}:${remote_path}
+scp ${local_path}/private_config.yaml ${remote_username}@${remote_host}:${remote_path}
