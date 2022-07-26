365883973d367ba5cbcdf995bd44984712d9dc8b
Bill-Haku <battleandwar@hotmail.com>
2022-07-26 11:59:40 +0800

    feat: update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,3 +2,4 @@ scp ~/pycharmprojects/qqpixivbot/main.py hakubill@bill.debian:~/ayaki
 scp ~/pycharmprojects/qqpixivbot/features.py hakubill@bill.debian:~/ayaki
 scp ~/pycharmprojects/qqpixivbot/events.txt hakubill@bill.debian:~/ayaki
 scp ~/pycharmprojects/qqpixivbot/config.yaml hakubill@bill.debian:~/ayaki
+scp ~/pycharmprojects/qqpixivbot/private_config.yaml hakubill@bill.debian:~/ayaki
